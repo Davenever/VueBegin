f7515fce6c4a190ec1f00853a797f7bdcd107e58
张韬 <18872046079@163.com>
2020-06-28 17:45:04 +0800

    add code

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,149 @@
 <template>
-  <div>Home组件</div>
+  <el-container>
+    <el-aside width="200px" id="aside">
+      <div class="adminInfo">江北的Admin Pro</div>
+      <div>
+        <el-row class="tac">
+          <el-col :span="12">
+            <el-menu
+              default-active="1-1"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose"
+              background-color="#20222A"
+              text-color="#fff"
+              active-text-color="#409EFF"
+            >
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template slot="title">分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </div>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <el-menu
+          :default-active="activeIndex2"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#393D49"
+          text-color="#fff"
+          active-text-color="#409EFF"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-s-fold"></i>
+          </el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="3" disabled>消息中心</el-menu-item>
+          <el-menu-item index="4">
+            <a href="https://www.ele.me" target="_blank">订单管理</a>
+          </el-menu-item>
+          <el-button type="primary" @click="loginOut">退出</el-button>
+          <i data-v-ad5d153c class="el-icon-arrow-down el-icon--right">111</i>
+        </el-menu>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1",
+      MenuList: []
+    };
+  },
+  methods: {
+    loginOut() {
+      alert(1);
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+  widows: 100%;
+}
+.el-menu-demo {
+  width: 100%;
+}
+.el-header {
+  padding: 0 0px;
+}
+.el-aside {
+  background-color: #20222a;
+  height: 100%;
+}
+#aside {
+  height: 100%;
+}
+.adminInfo {
+  height: 60px;
+}
+.tac {
+  height: 100%;
+}
+.el-menu-vertical-demo {
+  width: 200px;
+}
+.el-menu {
+  //这个是去除打开下拉时,宽度超出的问题
+  border-right: none;
+}
+</style>
